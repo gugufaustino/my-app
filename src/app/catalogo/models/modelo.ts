8b35c4ac49a106f17ad32197f0934cd6cf9351d5
Wellington <Wellington@GUGU-NOTE>
2022-03-22 23:08:42 -0300

    .

--- src/app/catalogo/models/modelo.ts
+++ src/app/catalogo/models/modelo.ts
@@ -3,15 +3,37 @@ import { MappingModel } from "src/app/base-contracts/models/mapping.model";
 export class Modelo implements MappingModel {
     constructor() { this.toMap(); }
     id: string;
+
+    //dados basicos
     nome: string;
+    dtNascimento: Date;
+    rg: string;
     cpf: string;
 
+    //tipo de modelo
+    diponibilidade: string;
+    tipoCasting: Number[];
+    
+    //contato
     email: string;
-    telefone: string;
+    telefone: string[];
+
+    //endereço
+    cep: string;
+    logradouro: string;
+    numero: number;
+    complemento: string;
+    bairro: string;
+    siglaUf: string;
+    nomeMunicipio: string;
+
 
     mappings: any[];
     toMap(): void {
-        //throw new Error("Method not implemented.");
+        this.mappings = [
+
+
+        ]
     }
 
 }
