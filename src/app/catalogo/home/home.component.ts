9bb859fed5632f0e74d09d81a5db5b8b71705308
Wellington <Wellington@GUGU-NOTE>
2022-04-12 22:56:50 -0300

    pesquisar catalogo

--- src/app/catalogo/home/home.component.ts
+++ src/app/catalogo/home/home.component.ts
@@ -1,8 +1,10 @@
+import { OptionSelect } from './../../app-core/models/option-select';
 import { Observable } from 'rxjs';
 import { CatalogoService } from './../services/catalogo.service';
 import { Modelo } from './../models/modelo';
 import { Component, OnInit } from '@angular/core';
 import { ToastAppService } from 'src/app/services/toastapp.service';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +14,36 @@ import { ToastAppService } from 'src/app/services/toastapp.service';
 export class HomeComponent implements OnInit {
 
   models : Observable<Modelo[]>;
+  componentForm: FormGroup;
+  controlsFormBase: any;
+  tipoCasting: OptionSelect[] = Modelo.tipoCastingEnum;
 
   constructor(
+    protected formBuilder: FormBuilder,
     private toastr: ToastAppService,
     private service: CatalogoService<Modelo>
-    ) { }
+    ) {
+
+      this.controlsFormBase = {
+        nome: ['',],
+        tipoCasting: this.buildTipoCasting(),
+
+      }
+     }
 
   ngOnInit(): void {
+    this.componentForm = this.formBuilder.group(this.controlsFormBase);
+
     this.models = this.service.listarTodos();
+  }
+
+  getTipoCastingControls() {
+    return this.componentForm.get('tipoCasting') ? (<FormArray>this.componentForm.get('tipoCasting')).controls : null;
+  }
+
+
+  buildTipoCasting() {
+    return this.formBuilder.array(this.tipoCasting.map(v => new FormControl(false)));
   }
 
 }
